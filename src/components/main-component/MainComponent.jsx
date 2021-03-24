d828a4ebfc2bef9084f9d9cc21a02fe796ca462d
Hugyn <hugoncc@hotmail.com>
2021-03-24 15:25:12 +0000

    Pages/about created

--- src/components/main-component/MainComponent.jsx
+++ src/components/main-component/MainComponent.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './MainComponent-styles.css'
 import NavbarComponent from './nav-bar/NavbarComponent'
-
+import AboutComponent from './pages/about/AboutComponent'
 function MainComponent() {
     return (
         <div className='mainComponent'>
             <NavbarComponent />
+            <AboutComponent/>
         </div>
     )
 }
